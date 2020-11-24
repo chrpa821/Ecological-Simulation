cc65de5c674ad615df2841467845b08c70d1b04f
Adam Moren <adam.moren1@gmail.com>
2020-11-24 09:55:10 +0100

    add environment class

--- Script/main.py
+++ Script/main.py
@@ -11,8 +11,7 @@ if 'myWin' in globals():
 ##############
 #   New UI   #
 ##############
-myWin = cmds.window(
-    title="Plant Distributions in Natural Scenes", menuBar=True)
+myWin = cmds.window(title="Plant Distributions in Natural Scenes", menuBar=True)
 
 cmds.frameLayout(collapsable=True, label="Create world")
 
@@ -20,10 +19,11 @@ cmds.frameLayout(collapsable=True, label="Create world")
 cmds.button(label="New Scene", command='new_scene()')
 
 # user set width and length
-cmds.intSliderGrp('input_width', label="Width",
-                  field=True, min=5, max=100, value=50)
-cmds.intSliderGrp('input_length', label="Length",
-                  field=True, min=5, max=100, value=50)
+cmds.intSliderGrp('input_width', label="Width", field=True, min=5, max=100, value=50)
+cmds.intSliderGrp('input_length', label="Length", field=True, min=5, max=100, value=50)
+
+# user set subdivisions
+cmds.intSliderGrp('input_sub', label="Number of Subdivisions", field=True, min=5, max=100, value=50)
 
 # Create new terrain
 cmds.button(label="Create New Terrain", command='create_plane()')
@@ -41,6 +41,7 @@ cmds.showWindow(myWin)
 mesh_name = ""
 mesh_width = 0
 mesh_length = 0
+mesh_sub = 0
 
 ##################
 #    Functions   #
@@ -56,8 +57,25 @@ def create_plane():
     mesh_width = cmds.intSliderGrp('input_width', query=True, value=True)
     global mesh_length
     mesh_length = cmds.intSliderGrp('input_length', query=True, value=True)
+    global mesh_sub
+    mesh_sub = cmds.intSliderGrp('input_sub', query=True, value=True)
 
     # Create a polygonal mesh
     global mesh_name
     mesh_name = "worldMesh"
-    cmds.polyPlane(width=mesh_width, height=mesh_length, name=mesh_name)
+    cmds.polyPlane(sx=mesh_sub, sy=mesh_sub,  w=mesh_width, h=mesh_length, n=mesh_name)
+
+##################
+#    Classes     #
+##################
+
+
+class Environment:
+    def __init__(self, age, sunlight, temperature):
+        self.age = age
+        self.sunlight = sunlight
+        self.temperature = temperature
+
+    def update(self):
+        print('Add TimeElement to update'.format(self.name))
+
